3e529e52a868f0d167917905595b76e1bf6bb2cb
satoshi yamada <bigt2323@gmail.com>
2017-01-09 17:10:49 +0900

    enable facebook login

--- Gemfile
+++ Gemfile
@@ -51,3 +51,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# for douzou_chan2
+gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'dotenv-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'welcome/home'
+  get "home", to: "home#index", as: "user_root"
+  root 'home#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
