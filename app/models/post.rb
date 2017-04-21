3d004606d5d302ac324ded15a367a2d110b3fe22
satoshi yamada <bigt2323@gmail.com>
2017-04-22 00:55:00 +0900

    change coordinate into lat and lon

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :coordinate, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
   validates :country, presence: true
   validates :administrative_area_level_1, presence: true
   validates :address, presence: true
