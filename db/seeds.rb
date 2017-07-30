83a96a33addb2581713fef03defe05253e16d74a
satoshi yamada <bigt2323@gmail.com>
2017-07-30 16:22:14 +0900

    issue #67

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,38 +14,3 @@ User.create!(email: "anonymous-user@railstutorial.org",
              password_confirmation: "foobar",
              admin: false)
 
-for n in 2..100
-  email = "douzou-chan-#{n+1}@test.org"
-  provider = n%2 == 0 ? "facebook" : "twitter"
-  uid = n
-  password = "foobar"
-  User.create!(email: email,
-               provider: provider,
-               uid: uid,
-               nickname: "anonymous#{uid}",
-               password:              password,
-               password_confirmation: password)
-end
-
-users = User.order(:created_at).take(6)
-50.times do |n|
-  country = n % 2 == 0 ? "日本" : "US"
-  administrative_area_level_1 = n % 2 == 0 ? "鹿児島県" : "CA"
-  ward = n % 2 == 0 ? "指宿市" : "Los Angeles"
-  address = n % 2 == 0 ? "日本 鹿児島県指宿市指宿1-1-1" : "Santa Monica, Los Angeles, CA, US"
-  author = n % 2 == 0 ? "運慶" : "Michaelangelo"
-  name = n % 2 == 0 ? "金剛力士像" : "Davide"
-  year = 1500
-  link = "http://www.yahoo.co.jp"
-  users.each { |user| user.posts.create!(latitude: "35.68#{n}#{n}".to_f,
-                                         longitude: "139.69#{n}#{n}55".to_f,
-										 country: country,
-										 administrative_area_level_1: administrative_area_level_1,
-										 ward: ward,
-                                         address: address,
-                                         author: author,
-                                         name: name,
-                                         year: year,
-                                         link: link
-  ) }
-end
--- memo.md
+++ memo.md
@@ -2,3 +2,23 @@ anonymous user id : 1
 
 postのuser_idはそのpostを作った人
 contribution の user_id はそのcontributionを作った人
+
+## データの投入について
+
+* プロダクション用のデータ投入
+```
+$ rails db:seed
+```
+
+* 開発用のデータ投入
+```
+$ rails db:seed:development
+```
+
+データの削除
+```
+$ rails console
+> User.delete_all
+> Post.delete_all
+> Pic.delete_all
+```
