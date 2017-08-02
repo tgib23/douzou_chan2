13d8b6014e324c38f32a9af38ab0aebad82a8349
satoshi yamada <bigt2323@gmail.com>
2017-08-02 22:55:38 +0900

    changes for running on vps

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.assets.precompile += ['*.js']
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
