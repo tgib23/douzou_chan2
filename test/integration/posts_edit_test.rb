bc3a8aaa309839eea24d75904162ee2a72851844
satoshi yamada <bigt2323@gmail.com>
2017-08-06 00:45:19 +0900

    issue #55

--- test/integration/posts_edit_test.rb
+++ test/integration/posts_edit_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class PostsEditTest < ActionDispatch::IntegrationTest
   def setup
     @post = posts(:two)
+    @user = User.find_by( uid: 102)
   end
 
-  test "post edit" do
-    get post_path(@post)
-  end
+#  test "post edit" do
+#    get post_path(@post)
+#  end
 end
