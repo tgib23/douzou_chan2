6f03d007c69c75fa93d13f6c7843fd94c15ecbde
satoshi yamada <bigt2323@gmail.com>
2017-09-14 23:42:23 +0900

    add user_id to pics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910135703) do
+ActiveRecord::Schema.define(version: 20170914143848) do
 
   create_table "contributions", force: :cascade do |t|
     t.integer  "user_id"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20170910135703) do
   create_table "pics", force: :cascade do |t|
     t.integer  "post_id"
     t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",    default: 1, null: false
   end
 
   create_table "posts", force: :cascade do |t|
