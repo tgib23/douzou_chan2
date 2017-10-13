5b8386b56859e7f7cc7dd83dc0cfe6f3b68e1e12
satoshi yamada <bigt2323@gmail.com>
2017-10-14 00:55:01 +0900

    issue 79

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,26 @@ class HomeController < ApplicationController
     redirect_to root_url unless admin_user?
   end
 
+  def near
+  end
+
+  def search_nearby
+
+    logger.debug params[:lat]
+    logger.debug params[:lon]
+    @latitude=params[:lat].to_f
+    @longitude=params[:lon].to_f
+    @nearby_posts = Post.where("latitude>=? AND latitude<?",@latitude-0.002,@latitude+0.002).where("longitude>=? AND longitude<?",@longitude-0.002,@longitude+0.002)
+
+    @nearby_hash= Gmaps4rails.build_markers(@nearby_posts) do |post, marker|
+      marker.lat post.latitude
+      marker.lng post.longitude
+      marker.infowindow post.name
+      marker.json({title: post.name})
+      marker.picture({url: 'http://maps.google.com/mapfiles/ms/icons/blue.png', width: 48, height: 48})
+    end
+  end
+
   private
     def get_and_show_posts
       @show_post = Post.paginate(page: params[:page], per_page: 5).order('created_at DESC')
